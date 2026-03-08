569e329b027a523eb72f2aecbdab13828d3dae10
DESKTOP-AVR0FVI\itou2 <itou.27x.kazu@gmail.com>
2026-03-08 18:26:45 +0900

    [ステージ] Stage4,End のシーンを一旦作成

---
+++ AnniversaryGame/Assets/Scenes/Stage4/Scripts/Stage4Flow.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine.InputSystem;
+using UniRx;
+
+public class Stage4Flow : BaseScene
+{
+    [SerializeField] GameObject _player;
+
+    [Header("初回ストーリーCSVファイル名")]
+    public string storyFileName = "";
+    [SerializeField, Header("初回ストーリーを表示するか")]
+    private bool m_isDispFirstStory;
+
+    // キーアクション
+    private InputAction _pushStart;
+
+    protected override async UniTask OnSceneReadyAsync(CancellationToken token)
+    {
+        // シーン起動時の処理
+        GameManager.Instance.StageName = "Stage2";
+
+        await UniTask.WaitUntil(() => GameManager.Instance.m_isInitializeResidentFlow);
+
+        // フェードアウト待ち
+        await GameManager.Instance.m_ResidentFlow.LoadingFadeOut();
+
+        // 初回ストーリー開始
+        if (m_isDispFirstStory)
+        {
+            await StartTextWindow(storyFileName, token);
+        }
+
+        // キー操作登録
+        GameManager.Instance.SetInputSystemAllDisable();
+        GameManager.Instance._InputControls.Player.Enable();
+
+        // Loading終了
+        await GameManager.Instance.m_ResidentFlow.LoadingFadeOut();
+    }
+}
